7cda5fb1a10e70c3d7375263e6b369ebfa10b70f
manrriqueMY <manrrique.my@gmail.com>
2020-08-20 18:14:38 -0500

    as

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> 
+        <style>
+            :root{
+                --color-primary:#ff0709;
+            }
+        </style>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://www.tgweb.spyrohost.com/css/style.css">
---
+++ resources/views/partial/descripcion.blade.php
@@ -0,0 +1,15 @@
+<div class="row">
+	<div class="col-md-8">
+		<hr size="2">
+		<h3 class="titulo"><strong>Descripción</strong></h3>
+		<br>
+		<div class="t-justify">
+			Al ser el monumento de pago más visitado del mundo, las colas de entradas para la Torre Eiffel son famosas por su longitud. No pierdas tiempo esperando y comienza a explorar de inmediato con las entradas de acceso prioritario de Klook. Conocerás a tu guía local y daréis un pequeño paseo hasta la Torre Eiffel. Una vez allí, subiréis directamente al ascensor. Si las alturas no te asustan, párate en el primer nivel y atrévete a caminar por el suelo transparente a 57 metros de altura. A continuación, coge un ascensor hasta la segunda planta donde disfrutarás de espectaculares vistas de París mientras tu guía te cuenta la fascinante historia que hay detrás de este extraordinario monumento. Si eliges el acceso a las 3 plantas, subirás a un
+			ascensor de cristal y verás cómo París se extiende ante ti a medida que subes gradualmente hasta la cima, a 276 metros de altura. Si eres valiente y decides subir serás recompensado con magníficas vistas, así que prepárate para sacar unas fotos inolvidables de la ciudad. Además, con las terrazas cerradas y al aire libre, podrás disfrutar de las vistas sin importar el clima o la hora del día. La Torre Eiffel, símbolo de Francia en todo el mundo, es una	visita imprescindible en París, así que ¿por qué no reservar ahora para entrar sin complicaciones?
+
+		</div>
+		<div>
+			<img src="https://www.tgweb.spyrohost.com/slider/79cfc2cfcc862d3c26486af26513077b.jpg" alt="Los Angeles" style="width:100%;">
+		</div>
+	</div>
+</div>
